99ab489b95d93bbcd2f117cc83da0935216f98a5
Insik Cho <iminsik@gmail.com>
2017-10-09 12:22:00 -0700

    add actions

---
+++ reactnd-project-client/src/actions/index.js
@@ -0,0 +1,13 @@
+export const VIEWPOSTS = "viewposts"
+export const DETAILPOST = "detailpost"
+
+export const ADDPOST= "addpost"
+export const EDITPOST = "editpost"
+
+export const ADDCOMMENT = "addcomment"
+export const EDITCOMMENT = "editcomment"
+
+export const INCVOTESCORE = "incvotescore"
+export const DECVOTESCORE = "decvotescore"
+
+
